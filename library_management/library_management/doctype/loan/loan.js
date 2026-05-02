096a0fb8a317dadf3ecb466afe2b35a5de39835e
Adil-km <adilkmparappur@gail.com>
2026-05-02 13:25:13 +0530

    feat: Added contstraint for adding books, client script to impove UX by populating member name and book title in Loan doctype form when their field changes.

--- library_management/library_management/doctype/loan/loan.js
+++ library_management/library_management/doctype/loan/loan.js
@@ -1,8 +1,31 @@
 // Copyright (c) 2026, Adil and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Loan", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Loan", {
+	issue_date : function(frm) {
+        let issue_date = frm.doc.issue_date
+        let days = 14
+        let due_date = frappe.datetime.add_days(issue_date, days)
+        frm.set_value("due_date", due_date)
+    },
+    book_id : function(frm){
+        if (frm.doc.book_id) {
+            frappe.db.get_value('Book Item', frm.doc.book_id, 'book')
+                .then(r => {
+                    let book_name = r.message.book;
+                    return  frappe.db.get_value('Book', book_name, 'title')
+                })
+                .then(r =>{
+                    frm.set_value("book_title",r.message.title)
+                });
+        }
+    },
+    member_id : function(frm){
+        if (frm.doc.member_id) {
+            frappe.db.get_value('Member', frm.doc.member_id, 'full_name')
+                .then(r =>{
+                    frm.set_value("member_name",r.message.full_name)
+                });
+        }
+    }
+});
--- library_management/library_management/doctype/loan/loan.py
+++ library_management/library_management/doctype/loan/loan.py
@@ -1,9 +1,23 @@
 # Copyright (c) 2026, Adil and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe.utils import add_to_date
 from frappe.model.document import Document
 
 
 class Loan(Document):
-	pass
+	def validate_book_availbale(self):
+		curr_book = f"{self.book_title} - {self.book_id}"
+		if frappe.get_doc("Book Item", self.book_id).status != "Available":
+			frappe.throw(f"{curr_book} is already Issued or not Available.")
+
+	def before_save(self):		
+		self.validate_book_availbale()
+
+	def on_submit(self):
+		self.validate_book_availbale()
+		frappe.db.set_value("Book Item", self.book_id, "status", "Issued")
+	
+	def on_cancel(self):
+		frappe.db.set_value("Book Item", self.book_id, "status", "Available")
